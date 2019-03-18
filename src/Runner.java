0b1a61382df2d6543b06da2ce743e7bdb5381e7d
Michael Radaszkiewicz <mrada6@gmail.com>
2019-03-18 19:52:37 -0400

    clean up unused code in new version

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,5 @@ public class Runner {
     public static void main(String[] args) {
         Board b = new Board();
         System.out.println(b);
-        PerftSuiteTwo p = new PerftSuiteTwo();
     }
 }
